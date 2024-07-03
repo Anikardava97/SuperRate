4d904bfd7770e951237529d2ad54c761b3b27980
Anikardava97 <145837570+Anikardava97@users.noreply.github.com>
2024-07-03 19:53:59 +0400

    Initial commit

---
+++ SuperRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperRate
+//
+//  Created by AnaKardava on 03.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
